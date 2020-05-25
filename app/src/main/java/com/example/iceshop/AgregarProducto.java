8b91ef409197c1729ed5a940b896d109924b951b
Your Name <sebascufu1705@hotmail.com>
2020-05-25 17:24:22 -0500

    cree la clase de las publicaciones

---
+++ app/src/main/java/com/example/iceshop/AgregarProducto.java
@@ -0,0 +1,14 @@
+package com.example.iceshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarProducto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_producto);
+    }
+}
